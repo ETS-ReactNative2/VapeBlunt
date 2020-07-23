f56e1f4a4010f9167665783c5ddff1bb6e16e8b5
thehaiwave <manueld0293@gmail.com>
2020-07-23 00:16:28 -0500

    Setup Blog tab and fixed data serving

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,19 +2,18 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {SafeAreaView,ScrollView,StyleSheet, View,Text,TouchableOpacity,Image } from 'react-native';
 
-
 const colors = require('../assets/colors');
 
-const BlogCard= ({ navigation }) => {
-
+function BlogCard(props) {
     return (
-        <View>
+        <View style={styles}>
             <View style={{alignItems:"center",flexDirection:"column",borderRadius:13,overflow:'hidden'}}>
-                <Image source={require('../assets/images/boundless.jpg')} style={{ height:350,backgroundColor:colors.lightgrey,resizeMode:'contain',borderColor:'green'}}/>
+                <Image source={{uri: props.source}} 
+                style={{aspectRatio:1, height: undefined, width: '100%', backgroundColor:colors.lightgrey, resizeMode:'cover', borderColor:'green'}}/>
             </View>
             <View>
-                <Text style={{fontWeight:"bold",fontSize:20,marginTop:20}}>Reseña Vaporizador CF Boundless</Text>
-                <Text style={{ color:'grey',opacity:.9}}>Es uno de los vaporizadores mas consistentes por este rango de precio, es fecil de usar y facil de limpiar </Text>
+                <Text style={{fontWeight:"bold",fontSize:20,marginTop:20}}> {props.title} </Text>
+                <Text style={{ color:'grey',opacity:.9}}>{props.description}</Text>
             </View>
             <TouchableOpacity style={{borderWidth:1,borderColor:'black',borderRadius:30,marginTop:20}}>
                 <Text style={{color:"black", textAlign: 'center',fontSize:22,alignSelf:'center',padding:10,fontWeight:'500'}}>Leer más</Text>
@@ -23,4 +22,10 @@ const BlogCard= ({ navigation }) => {
     )
 };
 
+const styles = StyleSheet.create({
+    marginBottom: 20,
+})
+
+
 export default BlogCard;
+
--- screens/Blog.js
+++ screens/Blog.js
@@ -1,21 +1,51 @@
-import * as React from 'react';
-import Header from '../components/Header';
-import {  View, Text, SafeAreaView} from 'react-native';
-import BlogCard from '../components/BlogCard';
-import { ScrollView } from 'react-native-gesture-handler';
+import * as React from "react";
+import Header from "../components/Header";
+import { View, Text, SafeAreaView } from "react-native";
+import BlogCard from "../components/BlogCard";
+import { ScrollView } from "react-native-gesture-handler";
+import { loadBlogsCollection, loadNewsCollection } from "../lib/mongodb-server";
 
-const Blog = ({navigation }) => {
+export default class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogItems: [],
+    };
+  }
 
-  return(
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-      <Header onPress = {()=>{navigation.navigate('Inicio')} } arrow/>
-      <View style={{flex: 1, padding: 16, backgroundColor:'white'}}>
-        <ScrollView>
-          <BlogCard/>
-        </ScrollView>
-      </View>
-    </SafeAreaView>
-  );
+  componentDidMount() {
+    loadBlogsCollection().then((res) => {
+      this.setState({ blogItems: res });
+    });
+  }
+
+  render() { 
+    let { navigation } = this.props;
+    var { blogItems } = this.state;
+    console.log(blogItems)
+    return ( 
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <Header
+          onPress={() => {
+            navigation.navigate("Inicio");
+          }}
+        />
+        <View style={{ flex: 1, padding: 16, backgroundColor: "white" }}>
+          <ScrollView>
+            {blogItems.map((blog) => {
+              return (
+                <BlogCard
+                  key={blog._id}
+                  title={blog.title}
+                  description={blog.description}
+                  source={blog.thumbnail}
+                />
+              );
+            })}
+            
+          </ScrollView>
+          </View>
+      </SafeAreaView> 
+    );
+  }
 }
-
-export default Blog;
