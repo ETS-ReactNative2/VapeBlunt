07ba5e6c0347c3a51b63ca4af3af63b9cb908b12
thehaiwave <manueld0293@gmail.com>
2020-07-23 03:18:08 -0500

    Added News tab

--- screens/Noticias.js
+++ screens/Noticias.js
@@ -1,21 +1,51 @@
-import * as React from 'react';
-import Header from '../components/Header';
-import {  View, SafeAreaView} from 'react-native';
-import BlogCard from '../components/BlogCard';
-import { ScrollView } from 'react-native-gesture-handler';
+import * as React from "react";
+import Header from "../components/Header";
+import { View, Text, SafeAreaView } from "react-native";
+import NoticiasCard from "../components/NoticiasCard";
+import { ScrollView } from "react-native-gesture-handler";
+import { loadNewsCollection } from "../lib/mongodb-server";
 
-const Noticias = (props) => {
-    var {navigation} = props
-    return(
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-            <Header onPress = {()=>{navigation.navigate('Inicio')} } arrow/>
-            <View style={{flex: 1, padding: 16, backgroundColor:'white'}}>
-                <ScrollView>
-                    <BlogCard/>
-                </ScrollView>
-            </View>
-        </SafeAreaView>
+export default class Noticias extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reportItems: [],
+    };
+  }
+
+  componentDidMount() {
+    loadNewsCollection().then((res) => {
+      this.setState({ reportItems: res });
+    });
+  }
+
+  render() { 
+    let { navigation } = this.props;
+    var { reportItems } = this.state;
+    console.log(reportItems)
+    return ( 
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <Header
+          onPress={() => {
+            navigation.navigate("Inicio");
+          }}
+        />
+        <View style={{ flex: 1, padding: 16, backgroundColor: "white" }}>
+          <ScrollView>
+            {reportItems.map((report) => {
+              return (
+                <NoticiasCard
+                  key={report._id}
+                  title={report.title}
+                  description={report.description}
+                  source={report.thumbnail}
+                />
+              );
+            })}
+            
+          </ScrollView>
+          </View>
+      </SafeAreaView> 
     );
+  }
 }
-
-export default Noticias;
