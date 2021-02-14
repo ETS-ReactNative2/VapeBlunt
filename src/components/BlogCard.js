a4dd9e17d506c6af6537ea8cd1e3eb2dffa68139
thehaiwave <manueld0293@gmail.com>
2021-02-14 09:25:20 -0600

    ALguien calle a ese puto perro ayuda

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,74 +1,81 @@
-import React from 'react';
+import React from "react";
 import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    Linking,
+} from "react-native";
 
-import { colors } from '../assets';
+import { colors } from "../assets";
 
 const BlogCard = (props) => {
-  const {
-    title,
-    content,
-    publishedAt,
-    image,
-    url,//open url on browser with RN linking library
-  } = props.article
+    const {
+        title,
+        content,
+        publishedAt,
+        image,
+        url, //open url on browser with RN linking library
+    } = props.article;
 
-  return (
-    <View style={[styles.container, props.style]}>
-      <Image source={{uri: image}} 
-        style={styles.image}
-        resizeMode='contain'
-      />
-      <Text style={styles.title}>
-        {title}
-      </Text>
-      <Text numberOfLines={3} style={{color:'grey', opacity: .9, textAlign: 'justify'}}>
-        {content}
-      </Text>
-      <TouchableOpacity style={styles.read_button}>
-        <Text style={{
-          color:"black",
-          textAlign: 'center',
-          fontSize: 22,
-          alignSelf:'center',
-          padding: 10,
-          fontWeight:'bold'
-        }}>
-          Leer más
-        </Text>
-      </TouchableOpacity>
-    </View>         
-  )
+    return (
+        <View style={[styles.container, props.style]}>
+            <Image
+                source={{ uri: image }}
+                style={styles.image}
+                resizeMode="contain"
+            />
+            <Text style={styles.title}>{title}</Text>
+            <Text
+                numberOfLines={3}
+                style={{ color: "grey", opacity: 0.9, textAlign: "justify" }}
+            >
+                {content}
+            </Text>
+            <TouchableOpacity
+                style={styles.read_button}
+                onPress={async () => await Linking.openURL(url)}
+            >
+                <Text
+                    style={{
+                        color: "black",
+                        textAlign: "center",
+                        fontSize: 22,
+                        alignSelf: "center",
+                        padding: 10,
+                        fontWeight: "bold",
+                    }}
+                >
+                    Leer más
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    height: 400,
-    marginBottom: 50,
-  },
-  image: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: colors.lightgrey,
-    borderColor: 'green',
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 20,
-    marginTop: 20,
-  },
-  read_button: {
-    borderWidth: 2,
-    borderColor: 'black',
-    borderRadius: 30,
-    marginTop: 20,
-  }
-})
+    container: {
+        height: 400,
+        marginBottom: 50,
+    },
+    image: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: colors.lightgrey,
+        borderColor: "green",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        marginTop: 20,
+    },
+    read_button: {
+        borderWidth: 2,
+        borderColor: "black",
+        borderRadius: 30,
+        marginTop: 20,
+    },
+});
 
 export default BlogCard;
-
--- src/screens/Checkout.js
+++ src/screens/Checkout.js
@@ -1,64 +1,154 @@
-import React from 'react';
-import { 
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-} from 'react-native';
-import {
-  SafeAreaView,
-  InputForm
-} from '../components';
+import React from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import { SafeAreaView, InputForm } from "../components";
+
+const useUserCheckoutInfo = () => {
+    const [info, setInfo] = React.useState({
+        email: "",
+        name: "",
+        lastName: "",
+        address: "",
+        postalCode: "",
+        country: "",
+        city: "",
+        state: "",
+        phoneNumber: "",
+    });
+
+    const setEmail = (email) => {
+        setInfo({ ...info, email: email });
+    };
+
+    const setName = (name) => {
+        setInfo({ ...info, name: name });
+    };
+
+    const setLastName = (lastName) => {
+        setInfo({ ...info, lastName: lastName });
+    };
+
+    const setAddress = (address) => {
+        setInfo({ ...info, address: address });
+    };
+
+    const setPostalCode = (postalCode) => {
+        setInfo({ ...info, postalCode: postalCode });
+    };
+
+    const setCountry = (country) => {
+        setInfo({ ...info, country: country });
+    };
+
+    const setCity = (city) => {
+        setInfo({ ...info, city: city });
+    };
+
+    const setState = (state) => {
+        setInfo({ ...info, state: state });
+    };
+
+    const setPhoneNumber = (phoneNumber) => {
+        setInfo({ ...info, phoneNumber: phoneNumber });
+    };
+
+    return [
+        info,
+        {
+            setEmail,
+            setName,
+            setLastName,
+            setAddress,
+            setPostalCode,
+            setCountry,
+            setCity,
+            setState,
+            setPhoneNumber,
+        },
+    ];
+};
 
 //Checkout screen
 const Checkout = (props) => {
-  return(
-    <SafeAreaView style={{
-        paddingHorizontal: 20,
-        paddingTop: 10,
-        backgroundColor: 'white'
-      }}
-    >
-      <Text style={styles.title}>
-        Información de Contacto
-      </Text>
-      <InputForm 
-      placeholder='Correo electrónico' titleStyle={{fontSize: 17}}/>
-      <Text style={styles.title}>
-        Dirección de Envío
-      </Text>
-      <View style={styles.container}>
-        <InputForm placeholder='Nombre' style={styles.inputForm}/>
-        <InputForm placeholder='Apellido' style={styles.inputForm}/>
-      </View>
-      <InputForm style={[styles.inputForm, {width: '100%', marginBottom: 10}]}
-      placeholder='Dirección Completa (Calle, Número Ext/Int, Colonia y/o Delegación)'/>
-      <View style={styles.container}>
-        <InputForm placeholder='Código Postal' style={styles.inputForm}/>
-        <InputForm placeholder='Ciudad' style={styles.inputForm}/>
-      </View>
-      <View style={styles.container}>
-        <InputForm placeholder='Estado' style={styles.inputForm}/>
-        <InputForm placeholder='País' style={styles.inputForm}/>
-      </View>
-      <InputForm placeholder='Teléfono'/>
-    </SafeAreaView>
-  )
-}
+    const [user, editors] = useUserCheckoutInfo();
+    console.log(user);
+
+    return (
+        <SafeAreaView
+            style={{
+                paddingHorizontal: 20,
+                paddingTop: 10,
+                backgroundColor: "white",
+            }}
+        >
+            <Text style={styles.title}>Información de Contacto</Text>
+            <InputForm
+                onChangeInput={(text) => editors.setEmail(text)}
+                placeholder="Correo electrónico"
+                titleStyle={{ fontSize: 17 }}
+            />
+            <Text style={styles.title}>Dirección de Envío</Text>
+            <View style={styles.container}>
+                <InputForm
+                    onChangeInput={(text) => editors.setName(text)}
+                    placeholder="Nombre"
+                    style={styles.inputForm}
+                />
+                <InputForm
+                    onChangeInput={(text) => editors.setLastName(text)}
+                    placeholder="Apellido"
+                    style={styles.inputForm}
+                />
+            </View>
+            <InputForm
+                onChangeInput={(text) => editors.setAddress(text)}
+                style={[styles.inputForm, { width: "100%", marginBottom: 10 }]}
+                placeholder="Dirección Completa (Calle, Número Ext/Int, Colonia y/o Delegación)"
+            />
+            <View style={styles.container}>
+                <InputForm
+                    onChangeInput={(text) => editors.setPostalCode(text)}
+                    placeholder="Código Postal"
+                    style={styles.inputForm}
+                />
+                <InputForm
+                    onChangeInput={(text) => editors.setCity(text)}
+                    placeholder="Ciudad"
+                    style={styles.inputForm}
+                />
+            </View>
+            <View style={styles.container}>
+                <InputForm
+                    onChangeInput={(text) => editors.setState(text)}
+                    placeholder="Estado"
+                    style={styles.inputForm}
+                />
+                <InputForm
+                    onChangeInput={(text) => editors.setCountry(text)}
+                    placeholder="País"
+                    style={styles.inputForm}
+                />
+            </View>
+            <InputForm
+                onChangeInput={(text) => editors.setPhoneNumber(text)}
+                placeholder="Teléfono"
+            />
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 18,
-    marginVertical: 10,
-  },
-  inputForm: {
-    width: '48%',
-  },
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  }
-})
+    title: {
+        fontSize: 18,
+        marginVertical: 10,
+    },
+    inputForm: {
+        width: "48%",
+    },
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+});
 
 export default Checkout;
