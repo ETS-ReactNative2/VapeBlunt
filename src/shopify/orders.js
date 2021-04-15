faf8e52421da110a554045561db555665224f23c
thehaiwave <manueld0293@gmail.com>
2021-04-15 06:18:05 -0500

    Fixed Shopify axios checkout request error I guess :(

--- src/shopify/orders.js
+++ src/shopify/orders.js
@@ -1,26 +1,26 @@
-
-import axios from 'axios';
-import { axiosOptions } from './config';
+import axios from "axios";
+import { axiosOptions } from "./config";
 
 export async function createCheckout(checkout_info) {
-  const {
-    email,
-    name,
-    lastName,
-    address,
-    postalCode,
-    city,
-    province,
-    country,
-    phoneNumber,
-    lineItems,
-  } = checkout_info;
-  //Query
-  axiosOptions.url = 'https://vapebluntmexico.myshopify.com/api/2021-04/graphql.json';
-  axiosOptions.headers['Content-Type'] = 'application/json'
+    const {
+        email,
+        name,
+        lastName,
+        address,
+        postalCode,
+        city,
+        province,
+        country,
+        phoneNumber,
+        lineItems,
+    } = checkout_info;
+    //Query
+    axiosOptions.url =
+        "https://vapebluntmexico.myshopify.com/api/2021-04/graphql.json";
+    axiosOptions.headers["Content-Type"] = "application/json";
 
-  axiosOptions.data = {
-    query: `mutation checkoutCreate($checkout: CheckoutCreateInput!) {
+    axiosOptions.data = {
+        query: `mutation checkoutCreate($checkout: CheckoutCreateInput!) {
       checkoutCreate(input: $checkout) {
         checkout {
           id
@@ -44,36 +44,37 @@ export async function createCheckout(checkout_info) {
         }
       }
     }`,
-    variables: {
-      "checkout": {
-        "email": `${email}`,
-        "lineItems": lineItems,
-        "shippingAddress": {
-          "firstName": `${name}`,
-          "lastName": `${lastName}`,
-          "address1": `${address}`,
-          "address2": ``,
-          "zip": `${postalCode}`,
-          "city": `${city}`,
-          "province": `${province}`,
-          "country": `${country}`,
-          "phone": `${phoneNumber}`
-        }
-      }
-    },
-    "operationName":"checkoutCreate"
-  }
-  //console.log("Sending to checkout", axiosOptions.data.variables);
-  axiosOptions.data = JSON.stringify(axiosOptions.data)
+        variables: {
+            checkout: {
+                email: `${email}`,
+                lineItems: lineItems,
+                shippingAddress: {
+                    firstName: `${name}`,
+                    lastName: `${lastName}`,
+                    address1: `${address}`,
+                    address2: ``,
+                    zip: `${postalCode}`,
+                    city: `${city}`,
+                    province: `${province}`,
+                    country: `${country}`,
+                    phone: `${phoneNumber}`,
+                },
+            },
+        },
+        operationName: "checkoutCreate",
+    };
+    //console.log("Sending to checkout", axiosOptions.data.variables);
+    axiosOptions.data = JSON.stringify(axiosOptions.data);
 
-  try{
-    const { data } = await axios(axiosOptions)
-    const { checkout, checkoutUserErrors } = data.data.checkoutCreate;
-    if(checkoutUserErrors.length > 0){
-      return Promise.reject(checkoutUserErrors)
+    try {
+        const { data } = await axios(axiosOptions);
+        axiosOptions.headers["Content-Type"] = "application/graphql";
+        const { checkout, checkoutUserErrors } = data.data.checkoutCreate;
+        if (checkoutUserErrors.length > 0) {
+            return Promise.reject(checkoutUserErrors);
+        }
+        return checkout;
+    } catch (e) {
+        return Promise.reject([e]);
     }
-    return checkout;
-  }catch(e){
-    return Promise.reject([e])
-  }
 }
--- src/shopify/products.js
+++ src/shopify/products.js
@@ -1,63 +1,56 @@
-import axios from 'axios';
-import { axiosOptions } from './config';
-import { destructureEdges } from './helpers';
+import axios from "axios";
+import { axiosOptions } from "./config";
+import { destructureEdges } from "./helpers";
 
-function extractImages(images){
-  if(!images){
-    return [];
-  }
-  const extractor = (node) => node.transformedSrc;
-  return destructureEdges(images.edges, extractor);
-}
-
-function extractVariants(variants){
-  const extractor = (node) => ({
-    ...node,
-    price: node.priceV2.amount,
-    image: node.image.transformedSrc,
-  })
-  return destructureEdges(variants.edges, extractor);
-}
-
-function populateProduct(product){
-  let {
-    title,
-    totalInventory,
-    images,
-    variants,
-    metafields,
-  } = product;
-  
-  if(images){
-    product.images = extractImages(images);
-    product.image = product.images[0];
-  }
-
-  if(variants){
-    product.variants = extractVariants(variants);
-  }
-  
-  let rating = 4;
-  if(metafields){
-    const nodes = destructureEdges(metafields);
-    for(let i=0; i < nodes.length; i++){
-      const node = nodes[i];
-      if (node.key === 'reviews_average') {
-        rating = parseFloat(node.value)
-        if (rating % 0.5 !== 0) {
-          rating = Math.round(rating)
-        }
-      }
+function extractImages(images) {
+    if (!images) {
+        return [];
     }
-  }
-  product.rating = rating;
-  return product;
+    const extractor = (node) => node.transformedSrc;
+    return destructureEdges(images.edges, extractor);
 }
 
+function extractVariants(variants) {
+    const extractor = (node) => ({
+        ...node,
+        price: node.priceV2.amount,
+        image: node.image.transformedSrc,
+    });
+    return destructureEdges(variants.edges, extractor);
+}
+
+function populateProduct(product) {
+    let { title, totalInventory, images, variants, metafields } = product;
+
+    if (images) {
+        product.images = extractImages(images);
+        product.image = product.images[0];
+    }
+
+    if (variants) {
+        product.variants = extractVariants(variants);
+    }
+
+    let rating = 4;
+    if (metafields) {
+        const nodes = destructureEdges(metafields);
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            if (node.key === "reviews_average") {
+                rating = parseFloat(node.value);
+                if (rating % 0.5 !== 0) {
+                    rating = Math.round(rating);
+                }
+            }
+        }
+    }
+    product.rating = rating;
+    return product;
+}
 
 export async function newProducts(first = 5) {
-  //Query
-  axiosOptions.data = `
+    //Query
+    axiosOptions.data = `
   query NewProducts{
     products(first: ${first}, reverse: true, query: "available_for_sale:true AND product_type:Vap*") {
       edges {
@@ -85,26 +78,25 @@ export async function newProducts(first = 5) {
         }
       }
     }
-  }`
+  }`;
 
-  try{
-    const { data } = await axios(axiosOptions)
-    const { edges } = data.data.products;
-    return destructureEdges(edges, populateProduct);
-  }catch(e){
-    console.log("Axios request error", e);
-    return [];
-  }
+    try {
+        const { data } = await axios(axiosOptions);
+        const { edges } = data.data.products;
+        return destructureEdges(edges, populateProduct);
+    } catch (e) {
+        console.log("Axios request error", e);
+        return [];
+    }
 }
 
-
 async function bestSellers(first = 10) {
-  return loadCollectionProducts('mas-vendidos', first);
+    return loadCollectionProducts("mas-vendidos", first);
 }
 
 async function loadCollectionProducts(handle, first = 250) {
-  //Query
-  axiosOptions.data = `
+    //Query
+    axiosOptions.data = `
   {
     collectionByHandle(handle: "${handle}") {
       products(first: ${first}, sortKey: BEST_SELLING) {
@@ -126,21 +118,21 @@ async function loadCollectionProducts(handle, first = 250) {
       }
     }
   }
-  `
+  `;
 
-  try{
-    const { data } = await axios(axiosOptions)
-    const { edges } = data.data.collectionByHandle.products;
-    return destructureEdges(edges, populateProduct);
-  }catch(e){
-    console.log("Axios request error", e);
-    return [];
-  }
+    try {
+        const { data } = await axios(axiosOptions);
+        const { edges } = data.data.collectionByHandle.products;
+        return destructureEdges(edges, populateProduct);
+    } catch (e) {
+        console.log("Axios request error", e);
+        return [];
+    }
 }
 
 export async function productInfo(handle) {
-  if(!handle) return Promise.reject('Handle not provided')
-  axiosOptions.data = `
+    if (!handle) return Promise.reject("Handle not provided");
+    axiosOptions.data = `
   {
     productByHandle(handle: "${handle}") {
       id
@@ -183,30 +175,30 @@ export async function productInfo(handle) {
       }
     }
   }
-  `
-  //return Promise with unwrapped data
-  try{
-    const {data} = await axios(axiosOptions);
-    let product = data.data.productByHandle;
-    return Promise.resolve(populateProduct(product));
-  }catch(e){
-    return Promise.reject(e)
-  }
+  `;
+    //return Promise with unwrapped data
+    try {
+        const { data } = await axios(axiosOptions);
+        let product = data.data.productByHandle;
+        return Promise.resolve(populateProduct(product));
+    } catch (e) {
+        return Promise.reject(e);
+    }
 }
 
-export async function getBlogs(){
-  return Promise.resolve([])
+export async function getBlogs() {
+    return Promise.resolve([]);
 }
 
-export async function getNews(){
-  return Promise.resolve([])
+export async function getNews() {
+    return Promise.resolve([]);
 }
 
 module.exports = {
-  newProducts,
-  bestSellers,
-  loadCollectionProducts,
-  productInfo,
-  getBlogs,
-  getNews,
-}
+    newProducts,
+    bestSellers,
+    loadCollectionProducts,
+    productInfo,
+    getBlogs,
+    getNews,
+};
